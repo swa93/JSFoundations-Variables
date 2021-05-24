48b1df56953a9feaed91e51d82fcea01dd6321cd
shahad <alansarishahad@gmail.com>
2021-05-24 18:38:43 +0300

    task done

---
+++ story.js
@@ -0,0 +1,5 @@
+const name = "laila";
+const age = "25";
+const hobby = "hoarding books";
+
+console.log(`Mr./Mrs. ${name} , in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`);
